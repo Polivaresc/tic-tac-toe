dafcad4d7d19ee6590fb937581053c7cc26742e6
Polivaresc <paulaolca98@gmail.com>
2022-01-12 23:05:42 +0100

    playerfactory, gamemodule, gameboardmodule

--- scripts.js
+++ scripts.js
@@ -1,76 +1,113 @@
 
-const gameboardModule = ((cellFactory) => {
+const gameboardModule = (() => {
     const gameboard = []
 
-    const createBoard = function () {
+    const createBoard = () => {
         const container = document.querySelector('.container')
         for (i = 0; i<3*3; i++) {
-            const cell = document.createElement('div')
-            cell.setAttribute('id', i)
-            cell.setAttribute('class', 'cell')
-            container.appendChild(cell)
+            const domCell = document.createElement('div')
+            domCell.setAttribute('id', 'cell-' + i)
+            domCell.classList = 'cell unmarked'
+            container.appendChild(domCell)
 
-            const Cell = Object.assign({}, cellFactory, {id: cell.id})
-            gameboard.push(Cell)
-        }
-    }
-    
-    return {createBoard}
-})();
-
-
-gameboardModule.createBoard()
-
-
-const playerModule = ((playerFactory) => {
-
-    const player1 = Object.assign({}, playerFactory, {symbol: 'X'})
-    const player2 = Object.assign({}, playerFactory, {symbol: 'O'})
-
-    function play() {
-        if (player1.round <= player2.round) {
-            const cell = document.querySelector('.cell')
-            cell.addEventListener('click', () => {
-                if (!cell.textContent)
-                cell.textContent = player1.symbol
+            domCell.addEventListener('click', () => {
+                const cell = gameboard.find(c => c.id === domCell.id)
+                game.mark(cell)
             })
+
+            gameboard.push({id: domCell.id, player: null})
         }
     }
 
-        // play: play() {
-        // player click a cell, take the cell id; 
-        // if the cell has a symbol already player must click another cell
-        // if the cell is empty put the player's symbol in that cell (change styles)
-    // }
+    const addSymbol = (cell) => {
+        const domCell = document.querySelector('#' + cell.id)
+        domCell.textContent = cell.player.getSymbol()
+        domCell.style.color = cell.player.getColor()
+        domCell.classList.remove('unmarked')
+    }
 
-    return {play}
-
+    const getGameboard = () => gameboard
+    
+    return {createBoard, addSymbol, getGameboard}
 })();
 
+const playerFactory = (name, symbol, color) => {
 
-// each cell has = {id, empty: true (if no player has clicked it yet), symbol: null if it's empty, X if it's one player, O if it's the other player}
-const cellFactory = {
-    symbol: function symbol(player) {
-        return player.symbol
-    },
-    empty: function isEmpty(symbol) {
-        if (!symbol) {
-            return true
-        }
-    }
-}
-
-const playerFactory = (name) => {
-    let score = 0
-    let round = 0
     const getName = () => name
+    const getSymbol = () => symbol
+    const getColor = () => color
 
     const win = () => {
-    
+        alert(`Player ${name} is the winner`)
+        location.reload()
     }
 
-    return {getName, win}
+    return {getName, getSymbol, getColor, win}
 }
 
+const game = (() => {
+    const player1 = playerFactory('Nyx', 'F', 'pink')
+    const player2 = playerFactory('Seitan', 'O', 'green')
+    let currentPlayer = player1
 
-// const results = {firstLine: [O, O, O], secondLine: [O, O, O], thirdLine: [O, O, O]}
+    const start = () => {
+        gameboardModule.createBoard()
+        announcePlayer()
+    }
+
+    const mark = (cell) => {
+        if (cell.player) {
+            alert('That cell is not available')
+            return
+        }
+
+        cell.player = currentPlayer
+        gameboardModule.addSymbol(cell)
+        if (checkWinner(currentPlayer)) {
+            currentPlayer.win()
+        } else {
+            const board = gameboardModule.getGameboard()
+
+            if (!board.find(c => c.player === null)) {
+                alert('Draw')
+                location.reload()
+            }
+            nextPlayer()
+        }
+    }
+
+    const nextPlayer = () => {
+        currentPlayer = ((currentPlayer === player1) ? player2 : player1)
+        announcePlayer()
+    }
+
+    const announcePlayer = () => {
+        const announcement = document.querySelector('#player-announce')
+        announcement.textContent = `${currentPlayer.getName()}'s turn!`
+        announcement.style.color = currentPlayer.getColor()
+    }
+
+    const checkWinner = (player) => {
+        const horizontal = [0, 3, 6].map(i => [i, i+1, i+2]);
+        const vertical = [0, 1, 2].map(i => [i, i+3, i+6]);
+        const diagonal = [[0, 4, 8], [2, 4, 6]];
+
+        const allWins = [].concat(horizontal).concat(vertical).concat(diagonal);
+        const board = gameboardModule.getGameboard()
+        
+        return allWins.some(indices => { 
+            const wins = board[indices[0]].player === player && board[indices[1]].player === player && board[indices[2]].player === player
+            if (wins) {
+                console.log(board[indices[0]])
+                console.log(board[indices[1]])
+                console.log(board[indices[2]])
+            }
+            return wins
+        })
+    }
+
+    return {start, mark}
+    
+})();
+
+game.start()
